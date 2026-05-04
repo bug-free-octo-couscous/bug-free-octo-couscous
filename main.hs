754d64bdaf30a0b99276de57e2066aee283390a4
jihoo <jihoop0620@gmail.com>
2026-05-05 08:44:30 +0900

    try pi types and universes

--- main.hs
+++ main.hs
@@ -7,7 +7,7 @@ import qualified Data.Set as Set
 import Data.List (intercalate)
 
 --------------------------------------------------------------------------------
--- 1. Interval Syntax & DNF (Your Original Logic)
+-- 1. Interval Syntax & DNF
 --------------------------------------------------------------------------------
 
 data I 
@@ -22,8 +22,7 @@ instance Show Literal where
     show (Pos n)    = "i" ++ show n
     show (NegVar n) = "¬i" ++ show n
 
-type Cube = Set Literal
-newtype DNF = DNF { getCubes :: Set Cube } deriving (Eq, Ord)
+newtype DNF = DNF { getCubes :: Set (Set Literal) } deriving (Eq, Ord)
 
 instance Show DNF where
     show (DNF cs) 
@@ -34,51 +33,70 @@ instance Show DNF where
         showCube c = if Set.null c then "1" else "(" ++ intercalate " ∧ " (map show (Set.toList c)) ++ ")"
 
 --------------------------------------------------------------------------------
--- 2. Lambda Calculus Syntax
+-- 2. Cubical Dependent Syntax
 --------------------------------------------------------------------------------
 
--- | Names for lambda variables
 type Name = String
+type Level = Int
 
 data Term
-    = TVar Name             -- Standard Lambda Variable
-    | TApp Term Term        -- Application (f x)
-    | TAbs Name Term        -- Lambda Abstraction (λx. t)
-    | TInterval I           -- An embedded Interval expression
-    | TCube DNF             -- An evaluated Interval (normalized)
-    deriving (Show)
+    = TVar Name
+    | TApp Term Term
+    | TAbs Name Term
+    -- Universes
+    | TUniv Level           -- U_n
+    -- Dependent Types (Pi Types)
+    | TPi Name Term Term    -- Π(x:A). B
+    -- Cubical Additions
+    | TInterval I
+    | TCube DNF
+    deriving (Eq)
+
+instance Show Term where
+    show t = case t of
+        TVar x      -> x
+        TApp f a    -> "(" ++ show f ++ " " ++ show a ++ ")"
+        TAbs x b    -> "λ" ++ x ++ ". " ++ show b
+        TUniv n     -> "U" ++ show n
+        TPi x a b   -> "Π(" ++ x ++ ":" ++ show a ++ "). " ++ show b
+        TInterval i -> show i
+        TCube c     -> show c
 
 --------------------------------------------------------------------------------
 -- 3. Evaluation & Substitution
 --------------------------------------------------------------------------------
 
--- | Simple substitution for Lambda Calculus: t[x := s]
+-- | Capture-avoiding substitution: t[x := s]
 subst :: Name -> Term -> Term -> Term
-subst x s t = case t of
+subst x s term = case term of
     TVar y      | x == y    -> s
                 | otherwise -> TVar y
     TApp f a                -> TApp (subst x s f) (subst x s a)
-    TAbs y body | x == y    -> TAbs y body -- Shadowing
-                | otherwise -> TAbs y (subst x s body)
+    TAbs y b    | x == y    -> TAbs y b
+                | otherwise -> TAbs y (subst x s b)
+    TPi y a b   | x == y    -> TPi y (subst x s a) b
+                | otherwise -> TPi y (subst x s a) (subst x s b)
+    TUniv n                 -> TUniv n
     TInterval i             -> TInterval i
     TCube c                 -> TCube c
 
--- | Normalizes both the Lambda structure and the Interval logic
-evalTerm :: Term -> Term
-evalTerm (TVar x) = TVar x
-evalTerm (TAbs x t) = TAbs x (evalTerm t)
-evalTerm (TApp f a) = 
-    case evalTerm f of
-        TAbs x body -> evalTerm (subst x (evalTerm a) body)
-        f'          -> TApp f' (evalTerm a)
-evalTerm (TInterval i) = TCube (evalInterval i) -- Normalize interval logic here
-evalTerm (TCube c)     = TCube c
+-- | Normalizes terms to Weak Head Normal Form / Normal Form
+eval :: Term -> Term
+eval t = case t of
+    TApp f a -> 
+        case eval f of
+            TAbs x body -> eval (subst x (eval a) body)
+            f'          -> TApp f' (eval a)
+    TAbs x b    -> TAbs x (eval b)
+    TPi x a b   -> TPi x (eval a) (eval b)
+    TInterval i -> TCube (evalInterval i)
+    _           -> t
 
 --------------------------------------------------------------------------------
--- 4. Interval Algebra (Refactored from your code)
+-- 4. Interval Algebra
 --------------------------------------------------------------------------------
 
-simplify :: Set Cube -> Set Cube
+simplify :: Set (Set Literal) -> Set (Set Literal)
 simplify cubes = Set.filter (\c -> not $ any (\other -> c /= other && other `Set.isSubsetOf` c) cubes) cubes
 
 evalInterval :: I -> DNF
@@ -89,7 +107,7 @@ evalInterval (Neg i)     = dnfNeg (evalInterval i)
 evalInterval (Meet i j)  = dnfMeet (evalInterval i) (evalInterval j)
 evalInterval (Join i j)  = dnfJoin (evalInterval i) (evalInterval j)
 
-dnfJoin (DNF a) (DNF b) = DNF $ simplify (Set.union a b)
+dnfJoin (DNF a) (DNF b)   = DNF $ simplify (Set.union a b)
 dnfMeet (DNF as) (DNF bs) = DNF $ simplify $ Set.fromList [ Set.union a b | a <- Set.toList as, b <- Set.toList bs ]
 dnfNeg (DNF cubes) 
     | Set.null cubes = DNF $ Set.singleton Set.empty
@@ -105,33 +123,30 @@ dnfNeg (DNF cubes)
 
 main :: IO ()
 main = do
-    putStrLn "=== Lambda Calculus + Cubical Intervals ==="
+    putStrLn "=== Cubical Lambda Calculus with Universes ==="
 
-    -- Define an interval expression: (i0 ∨ ¬i0)
-    let intervalExpr = Join (IVar 0) (Neg (IVar 0))
+    -- 1. Identity function for a type in U0
+    -- id : Π(A:U0). Π(x:A). A
+    let idType = TPi "A" (TUniv 0) (TPi "x" (TVar "A") (TVar "A"))
+    let idTerm = TAbs "A" (TAbs "x" (TVar "x"))
     
-    -- Define a lambda: λx. (x ∧ i1)
-    -- In cubical terms, this could represent a path transformation
-    let lambdaPath = TAbs "x" (TApp (TVar "x") (TInterval I1))
+    putStrLn $ "\nIdentity Type: " ++ show idType
+    putStrLn $ "Identity Term: " ++ show idTerm
 
-    -- Example 1: Identity Application
-    -- (λx. x) (i0 ∨ ¬i0)
-    let identity = TAbs "x" (TVar "x")
-    let test1 = TApp identity (TInterval intervalExpr)
-    
-    putStrLn $ "Input:  (λx. x) (i0 ∨ ¬i0)"
-    putStrLn $ "Result: " ++ show (evalTerm test1)
+    -- 2. Applying identity to a Universe
+    -- ((λA. λx. x) U0) -> λx. x
+    let testUniv = TApp idTerm (TUniv 0)
+    putStrLn $ "\nApplying id to U0:"
+    putStrLn $ "Result: " ++ show (eval testUniv)
 
-    -- Example 2: Constant Function
-    -- (λx. i0) (i1)
-    let constFunc = TAbs "x" (TInterval I0)
-    let test2 = TApp constFunc (TInterval I1)
-    
-    putStrLn $ "\nInput:  (λx. i0) (i1)"
-    putStrLn $ "Result: " ++ show (evalTerm test2)
+    -- 3. Path-like type: A function from the interval to a universe
+    -- This represents a "Line" of types: Π(i:I). U0
+    let lineType = TPi "i" (TInterval (IVar 0)) (TUniv 0)
+    putStrLn $ "\nLine of Types (Path in U0): " ++ show lineType
 
-    -- Example 3: Nested Normalization
-    -- Applying a double negation to a variable within a lambda
-    let test3 = TInterval (Neg (Neg (IVar 5)))
-    putStrLn $ "\nInput:  ¬¬i5"
-    putStrLn $ "Result: " ++ show (evalTerm test3)
+    -- 4. Cubical Normalization inside types
+    -- Π(x : ¬¬i0). U0  ==> Π(x : i0). U0
+    let nestedLogic = TPi "x" (TInterval (Neg (Neg (IVar 0)))) (TUniv 0)
+    putStrLn $ "\nNormalized Interval in Pi-binder:"
+    putStrLn $ "Input:  " ++ show nestedLogic
+    putStrLn $ "Result: " ++ show (eval nestedLogic)
